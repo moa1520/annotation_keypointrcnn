3b92f32fb1c1b0bb6983afd6547d38e281b217e5
taekuyng kang <douit_be@naver.com>
2021-07-12 12:29:16 +0900

    revised

--- divide.py
+++ divide.py
@@ -62,93 +62,101 @@ def pose(keys, image_id, image_name):
 
 
 def main():
-    json_file = 'f_00001.json'
-    with open(json_file) as f:
-        obj = json.load(f)
+    root = '/Users/taekyoungkang/Desktop/TK/front_json'
+    for file in sorted(os.listdir(root)):
+        json_file = os.path.join(root, file)
+        with open(json_file) as f:
+            obj = json.load(f)
 
-    bbox_obj = dict()
-    image_name_obj = dict()
+        bbox_obj = dict()
+        image_name_obj = dict()
 
-    for i in range(len(obj['annotations'])):
-        image_id = obj['annotations'][i]['image_id']
-        # body
-        if 'num_keypoints' in obj['annotations'][i].keys():
-            image_name = obj['images'][i]['url'].split('/')[-1].split('.')[0]
-            body_keypoints = obj['annotations'][i]['keypoints']
-            body_bbox = obj['annotations'][i]['bbox']
-            pose_json = pose(body_keypoints, image_id=image_id,
-                             image_name=image_name)
-            # pose_json = json.dumps(pose_json)
+        for i in range(len(obj['annotations'])):
+            image_id = obj['annotations'][i]['image_id']
+            # body
+            if 'num_keypoints' in obj['annotations'][i].keys():
+                image_name = obj['images'][i]['coco_url'].split(
+                    '/')[-1].split('.')[0]
+                body_keypoints = obj['annotations'][i]['keypoints']
+                body_bbox = obj['annotations'][i]['bbox']
+                pose_json = pose(body_keypoints, image_id=image_id,
+                                 image_name=image_name)
+                # pose_json = json.dumps(pose_json)
 
-            # pose json파일 저장
-            with open('pose_json/{}.json'.format(i), 'w', encoding='utf-8') as make_file:
-                json.dump(pose_json, make_file, indent='\t')
-            #
+                # pose json파일 저장
+                if not os.path.isdir('/Volumes/SSD_250G/ktk/pose/{}'.format(file.split('.')[0])):
+                    os.makedirs(
+                        '/Volumes/SSD_250G/ktk/pose/{}'.format(file.split('.')[0]))
+                with open('/Volumes/SSD_250G/ktk/pose/{}/{}.json'.format(file.split('.')[0], image_name), 'w', encoding='utf-8') as make_file:
+                    json.dump(pose_json, make_file, indent='\t')
+                #
 
-            bbox_obj[image_id] = {'person_bb': body_bbox}
-            image_name_obj[image_id] = image_name
+                bbox_obj[image_id] = {'person_bb': body_bbox}
+                image_name_obj[image_id] = image_name
 
-        # ball, club head
-        else:
-            image_name = image_name_obj[image_id]
-            bbox = obj['annotations'][i]['bbox']
-            category_id = obj['annotations'][i]['category_id']
+            # ball, club head
+            else:
+                image_name = image_name_obj[image_id]
+                bbox = obj['annotations'][i]['bbox']
+                category_id = obj['annotations'][i]['category_id']
 
-            if category_id == '2':  # club_bb
-                bbox_obj[image_id]['club_bb'] = bbox
-            else:  # ball_bb
-                bbox_obj[image_id]['ball_bb'] = bbox
+                if category_id == '2':  # club_bb
+                    bbox_obj[image_id]['club_bb'] = bbox
+                else:  # ball_bb
+                    bbox_obj[image_id]['ball_bb'] = bbox
 
-    all_bbox = []
-    for i in range(len(obj['images'])):
-        club_count = 0
-        ball_count = 0
-        image_id = obj['annotations'][i]['image_id']
-        image_name = obj['images'][i]['url'].split('/')[-1].split('.')[0]
-        if 'club_bb' in bbox_obj[image_id].keys():
-            club_count = len(bbox_obj[image_id]['club_bb']) // 4
-        if 'ball_bb' in bbox_obj[image_id].keys():
-            ball_count = len(bbox_obj[image_id]['ball_bb']) // 4
+        all_bbox = []
+        for i in range(len(obj['images'])):
+            club_count = 0
+            ball_count = 0
+            image_id = obj['annotations'][i]['image_id']
+            image_name = obj['images'][i]['coco_url'].split(
+                '/')[-1].split('.')[0]
+            if 'club_bb' in bbox_obj[image_id].keys():
+                club_count = len(bbox_obj[image_id]['club_bb']) // 4
+            if 'ball_bb' in bbox_obj[image_id].keys():
+                ball_count = len(bbox_obj[image_id]['ball_bb']) // 4
 
-        info_set = {
-            "info": {
-                "image_name": image_name + ".png",
-                "image_id": image_id,
-                "person_count": 1,
-                "club_count": club_count,
-                "ball_count": ball_count
+            info_set = {
+                "info": {
+                    "image_name": image_name + ".png",
+                    "image_id": image_id,
+                    "person_count": 1,
+                    "club_count": club_count,
+                    "ball_count": ball_count
+                }
             }
+
+            if 'club_bb' in bbox_obj[image_id].keys():
+                club_bb = bbox_obj[image_id]['club_bb']
+            else:
+                club_bb = -1
+            if 'ball_bb' in bbox_obj[image_id].keys():
+                ball_bb = bbox_obj[image_id]['ball_bb']
+            else:
+                ball_bb = -1
+
+            bb_set = {
+                "bb": {
+                    "image_id": image_id,
+                    "person_bb": bbox_obj[image_id]['person_bb'],
+                    "club_bb": club_bb,
+                    "ball_bb": ball_bb
+                }
+            }
+
+            all_bbox.append(info_set)
+            all_bbox.append(bb_set)
+
+        all_bbox = {
+            "swing{}_detect.json".format(
+                str(int(json_file.split('_')[-1].split('.')[0][-4:])).zfill(4)): all_bbox
         }
 
-        if 'club_bb' in bbox_obj[image_id].keys():
-            club_bb = bbox_obj[image_id]['club_bb']
-        else:
-            club_bb = -1
-        if 'ball_bb' in bbox_obj[image_id].keys():
-            ball_bb = bbox_obj[image_id]['ball_bb']
-        else:
-            ball_bb = -1
-
-        bb_set = {
-            "bb": {
-                "image_id": image_id,
-                "person_bb": bbox_obj[image_id]['person_bb'],
-                "club_bb": club_bb,
-                "ball_bb": ball_bb
-            }
-        }
-
-        all_bbox.append(info_set)
-        all_bbox.append(bb_set)
-
-    all_bbox = {
-        "swing{}_detect.json".format(
-            str(int(json_file.split('_')[-1].split('.')[0])).zfill(4)): all_bbox
-    }
-
-    # bbox json 파일 저장
-    with open('test.json', 'w', encoding='utf-8') as make_file:
-        json.dump(all_bbox, make_file, indent='\t')
+        # bbox json 파일 저장
+        with open('/Volumes/SSD_250G/ktk/bbox/{}.json'.format(file.split('.')[0]), 'w', encoding='utf-8') as make_file:
+            json.dump(all_bbox, make_file, indent='\t')
+        print(file + "is Done!")
 
 
 if __name__ == '__main__':
